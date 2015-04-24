003a733e0da9ae44155b77e9da0f9246e0510418
Will Koehler <wck555@gmail.com>
2015-04-24 11:41:45 -0400

    Change backup time so it's late at night EST
    OpsWorks doesn't have a mechanism for setting the server time zone. Best practice is to run all server operations, like cron, on UTC.

--- mysql/recipes/backup.rb
+++ mysql/recipes/backup.rb
@@ -5,7 +5,7 @@ template "/srv/backup" do
 end
 
 cron "backup_mysql" do
-  hour "2"
+  hour "6"
   minute "0"
   command "/srv/backup >> /var/log/aws/opsworks/backup.log 2>&1"
   path "/usr/local/bin"
